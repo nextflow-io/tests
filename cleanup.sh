683ad0b701aba354a6d09142efc21555cbb19087
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-10-12 11:29:42 +0200

    Updated cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,5 @@ find . -name stdout | xargs rm
 find . -name .stdout | xargs rm
 find . -name checks.out | xargs rm
 find . -name .cache | xargs rm -rf
-find . -name '.nextflow*' | xargs rm 
+find . -name '.nextflow*' | xargs rm -rf  
 rm -rf checks/escape-globs.nf/[work-dir]/
